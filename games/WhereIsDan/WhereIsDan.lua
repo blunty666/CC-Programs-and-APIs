7bbdaaaa9d1802b0aff5bcad369e4e621b9cc106
blunty666 <matthewwblunt@gmail.com>
2018-06-29 18:56:23 +0100

    Added WhereIsDan game / screensaver

---
+++ games/WhereIsDan/WhereIsDan.lua
@@ -0,0 +1,187 @@
+local BOX = {
+	WIDTH = 20,
+	HEIGHT = 7,
+}
+
+local BUTTON = {
+	WIDTH = 18,
+}
+
+local URL = "https://raw.githubusercontent.com/blunty666/CC-Programs-and-APIs/master/games/WhereIsDan/quotes.lua"
+
+local WIDTH, HEIGHT = term.getSize()
+local CENTER_X, CENTER_Y = math.ceil(WIDTH/2), math.ceil(HEIGHT/2)
+
+do
+	local DELTA_X, DELTA_Y = math.floor(BOX.WIDTH/2), math.floor(BOX.HEIGHT/2)
+	
+	BOX.X_MIN = CENTER_X - DELTA_X
+	BOX.X_MAX = BOX.X_MIN + BOX.WIDTH - 1
+	
+	BOX.Y_MIN = CENTER_Y - DELTA_Y
+	BOX.Y_MAX = BOX.Y_MIN + BOX.HEIGHT - 1
+
+	BOX.BUTTON_TEXT = {
+		X = CENTER_X,
+		Y = BOX.Y_MIN + 2,
+	}
+
+	BOX.BUTTON = {
+		X_MIN = CENTER_X - math.floor(BUTTON.WIDTH/2),
+		X_MAX = CENTER_X - math.floor(BUTTON.WIDTH/2) + BUTTON.WIDTH - 1,
+
+		Y_MIN = BOX.Y_MIN + 1,
+		Y_MAX = BOX.Y_MIN + 3,
+	}
+
+	BOX.TEXT = {
+		X = CENTER_X,
+		Y = BOX.Y_MIN + 5,
+	}
+end
+
+local CHANGE_PROBABILITY = 0.15
+
+local particles = {}
+
+local function randomColour()
+	return 2^(math.random(0, 14))
+end
+
+local directions = {
+	[0] = vector.new(0, -1, 0),
+	[1] = vector.new(1, 0, 0),
+	[2] = vector.new(0, 1, 0),
+	[3] = vector.new(-1, 0, 0),
+}
+
+local function newParticle(x, y, colour, directionIndex)
+	return {
+		position = vector.new(x, y, 0),
+		colour = colour,
+		directionIndex = directionIndex,
+		direction = directions[directionIndex],
+		distanceTravelled = 0,
+	}
+end
+
+local function isInTerm(x, y)
+	return 1 <= x and x <= WIDTH and 1 <= y and y <= HEIGHT
+end
+
+local function shouldChangeDirection(particle)
+	return math.random() < CHANGE_PROBABILITY
+end
+
+local function shouldDraw(particle)
+	local x, y = particle.position.x, particle.position.y
+	return not (BOX.X_MIN <= x and x <= BOX.X_MAX and BOX.Y_MIN <= y and y <= BOX.Y_MAX)
+end
+
+local function draw(particle)
+	term.setCursorPos(particle.position.x, particle.position.y)
+	term.setBackgroundColour(particle.colour)
+	term.write(" ")
+end
+
+local function updateParticles()
+	while true do
+		for _, particle in ipairs(particles) do
+			local nextPosition = particle.position + particle.direction
+			if not isInTerm(nextPosition.x, nextPosition.y) or shouldChangeDirection(particle) then
+
+				local directionDelta = (math.random() > 0.5 and 1) or -1
+				local newDirectionIndex, newDirection = particle.directionIndex, nil
+				repeat
+					newDirectionIndex = (newDirectionIndex + directionDelta) % 4
+					newDirection = directions[newDirectionIndex]
+					nextPosition = particle.position + newDirection
+				until isInTerm(nextPosition.x, nextPosition.y)
+				
+				-- update direction
+				particle.directionIndex = newDirectionIndex
+				particle.direction = newDirection
+
+				-- reset distanceTravelled
+				particle.distanceTravelled = 0
+			end
+
+			-- move particle
+			particle.position = nextPosition
+
+			-- increase distanceTravelled
+			particle.distanceTravelled = particle.distanceTravelled + 1
+
+			-- check if should draw particle and draw
+			if shouldDraw(particle) then
+				draw(particle)
+			end
+		end
+		sleep(0.05)
+	end
+end
+
+local function writeText(xPos, yPos, text, bgCol, textCol)
+	if bgCol then term.setBackgroundColour(bgCol) end
+	if textCol then term.setTextColour(textCol) end
+	term.setCursorPos(xPos, yPos)
+	term.write(text)
+end
+
+local function writeTextAligned(text, xPos, yPos, bgCol, textCol)
+	local startX = xPos - math.floor(#text/2)
+	writeText(startX, yPos, text, bgCol, textCol)
+end
+
+local function drawButton(colour)
+	paintutils.drawFilledBox(BOX.BUTTON.X_MIN, BOX.BUTTON.Y_MIN, BOX.BUTTON.X_MAX, BOX.BUTTON.Y_MAX, colour)
+	writeTextAligned("WHERE IS DAN?", BOX.BUTTON_TEXT.X, BOX.BUTTON_TEXT.Y, colour, colours.white)
+end
+
+local function handleEvents()
+	local clicked = false
+	local event = {os.pullEvent()}
+	while true do
+		if event[1] == "key" then
+			if event[2] == keys.backspace then
+				break
+			end
+		elseif event[1] == "mouse_click" then
+			if event[2] == 1 and (BOX.BUTTON.X_MIN <= event[3] and event[3] <= BOX.BUTTON.X_MAX) and (BOX.BUTTON.Y_MIN <= event[4] and event[4] <= BOX.BUTTON.Y_MAX) then
+				clicked = true
+				drawButton(colours.red)
+			end
+		elseif event[1] == "mouse_up" then
+			if clicked then
+				drawButton(colours.green)
+				http.request(URL)
+				clicked = false
+			end
+		elseif event[1] == "http_success" and event[2] == URL then
+			local quotes = textutils.unserialise(event[3].readAll())
+			writeText(BOX.X_MIN, BOX.TEXT.Y, string.rep(" ", BOX.WIDTH), colours.black)
+			writeTextAligned(quotes[math.random(1, #quotes)], BOX.TEXT.X, BOX.TEXT.Y, colours.black, colours.white)
+		end
+		event = {os.pullEvent()}
+	end
+end
+
+term.setBackgroundColour(colours.black)
+term.clear()
+
+drawButton(colours.green)
+
+for i = 1, 25 do
+	local x = math.random(1, WIDTH)
+	local y = math.random(1, HEIGHT)
+	local colour = randomColour()
+	local directionIndex = math.random(0, 3)
+	table.insert(particles, newParticle(x, y, colour, directionIndex))
+end
+
+parallel.waitForAny(updateParticles, handleEvents)
+
+term.setBackgroundColour(colours.black)
+term.setTextColour(colours.white)
+term.setCursorPos(1, 1)
+term.clear()
